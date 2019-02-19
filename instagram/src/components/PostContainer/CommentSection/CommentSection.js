de9fec42cefb13e7b00557434bb6c5cfa452f67b
x-zen <jdharris.x@gmail.com>
2019-02-19 13:56:07 -0600

    Comment skeleton up

--- instagram/src/components/PostContainer/CommentSection/CommentSection.js
+++ instagram/src/components/PostContainer/CommentSection/CommentSection.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import Comment from './Comment.js'
+import CommentForm from './CommentForm.js'
 
-const CommentSection = props => {
-  return(
-    <div class='cmt'>
-      <p>CommentSection</p>
-    </div>
-  );
-};
+class CommentSection extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comments: props.comments
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.state.comments.map((c, i) => <Comment key={i} comment={c} />)}
+        <CommentForm />
+      </div>
+    );
+  }
+}
 
 export default CommentSection;
