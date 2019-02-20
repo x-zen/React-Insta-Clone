817edb315dddbdbd3bff5fb77bc958ce8b603eb1
x-zen <jdharris.x@gmail.com>
2019-02-20 15:44:57 -0600

    PostsPage

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,24 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 import dummyData from './dummy-data'
-import SearchBar from './components/SearchBar/SearchBar.js'
-import PostContainer from './components/PostContainer/PostContainer.js'
+import PostsPage from './components/PostsPage.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      posts: dummyData
+      posts: []
     };
   }
+
+  componentDidMount() {
+    this.setState({ posts: dummyData });
+  }
+
   render() {
     return (
-      <div className="App">
-        <SearchBar />
-        <PostContainer posts={this.state.posts}/>
-      </div>
+      <PostsPage />
     );
   }
+
 }
 
 export default App;
